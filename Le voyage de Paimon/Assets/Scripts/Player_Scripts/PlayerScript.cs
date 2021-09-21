2ea5f76123b512857bfc6c26a58a9b4c5b33ac15
IglooSyrup <darckuvina@gmail.com>
2021-09-21 15:37:54 -0400

    Scripts - Modification du player script

--- Le voyage de Paimon/Assets/Scripts/Player_Scripts/PlayerScript.cs
+++ Le voyage de Paimon/Assets/Scripts/Player_Scripts/PlayerScript.cs
@@ -6,44 +6,50 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     [SerializeField] private float mSpeed = 7.5f;
-    private const float PlayerSize = 0.4f; 
+    private const float PlayerSize = 0.4f;
+    private const float DefaultHealth = 200f;
     private Rigidbody2D _rigidbody2D;
     private Vector2 _mvmnt;
+    private float _playerHp;
+    private Transform _playerTransform;
     
 
     private void Start()
     {
         _rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+        _playerTransform = transform;
     }
 
     // Update is called once per frame
     private void Update()
     {
+        
         if (Input.GetKey("a"))
         {
             var x = mSpeed * Time.deltaTime; 
-            transform.position += Vector3.left * x;
-            transform.localScale = new Vector3(-PlayerSize, PlayerSize, 0f);
+            _playerTransform.position += Vector3.left * x;
+            _playerTransform.localScale = new Vector3(-PlayerSize, PlayerSize, 0f);
+            
         }
 
         if (Input.GetKey("d"))
         {
             var x = mSpeed * Time.deltaTime; 
-            transform.position += Vector3.right * x;           
-            transform.localScale = new Vector3(PlayerSize, PlayerSize, 0f);
+            _playerTransform.position += Vector3.right * x;           
+            _playerTransform.localScale = new Vector3(PlayerSize, PlayerSize, 0f);
             
         }
         
         if (Input.GetKey("w"))
         {
             var y = mSpeed * Time.deltaTime; 
-            transform.position += Vector3.up * y;
+            _playerTransform.position += Vector3.up * y;
         }
 
         if (Input.GetKey("s"))
         {
             var y = mSpeed * Time.deltaTime; 
-            transform.position += Vector3.down * y;
+            _playerTransform.position += Vector3.down * y;
         }
     }
 
