4412667acf8e7ce97e84a580e3855d2b1470f3f4
IglooSyrup <darckuvina@gmail.com>
2021-09-25 18:11:24 -0400

    Scripts - Désactivation de WolfWeapon lorsque le jeu débute

--- Le voyage de Paimon/Assets/Scripts/Attack_Scripts/WolfWeapon.cs
+++ Le voyage de Paimon/Assets/Scripts/Attack_Scripts/WolfWeapon.cs
@@ -7,13 +7,16 @@ public class WolfWeapon : MonoBehaviour
 
     [SerializeField] private GameObject wolfWeapon;
     private PolygonCollider2D _wolfCollider2D;
+    private SpriteRenderer _wolfSpriteRenderer;
     private Animator _animator;
     
     
     private void Start()
     {
         _animator = GetComponent<Animator>();
+        wolfWeapon.SetActive(false);
         _wolfCollider2D = wolfWeapon.GetComponent<PolygonCollider2D>();
+        _wolfSpriteRenderer = wolfWeapon.GetComponent<SpriteRenderer>();
         _wolfCollider2D.enabled = false;
     }
 
