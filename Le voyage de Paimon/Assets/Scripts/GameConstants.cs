d8a1656600d4f622cde391c7f55d39962bdcc19e
IglooSyrup <darckuvina@gmail.com>
2021-09-25 18:46:23 -0400

    Scripts - Ajout d'un script contenant toutes les constantes du jeu

---
+++ Le voyage de Paimon/Assets/Scripts/GameConstants.cs
@@ -0,0 +1,15 @@
+public class GameConstants
+{
+    public const string WolfWeaponTag = "WolfWeapon";
+    public const string BackWeaponTag = "BackWeapon";
+    public const string IsTouched = "isTouched";
+    public const string PlayerTag = "Player";
+    public const float ItemDelay = 0.1f;
+    public const float MonsterDelay = 0.3f;
+    public const string Attack01 = "j";
+    public const string Attack02 = "k";
+    public const string Attack03 = "l";
+    public const string UltAttack = "space";
+
+}
+
