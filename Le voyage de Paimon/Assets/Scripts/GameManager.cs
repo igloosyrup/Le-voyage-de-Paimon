2315ffd944059d073b45e515c8b96c0611f32f4e
IglooSyrup <darckuvina@gmail.com>
2021-10-03 01:43:25 -0400

    Scripts - Modification + Ajout des lignes pour loader une scène

--- Le voyage de Paimon/Assets/Scripts/GameManager.cs
+++ Le voyage de Paimon/Assets/Scripts/GameManager.cs
@@ -1,25 +1,77 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
 {
+    private static GameManager _gameManagerInstance;
+    public static GameManager getGameManagerInstance => _gameManagerInstance;
+    
     [SerializeField] private List<AudioClip> mainMenuAudioClips;
     [SerializeField] private List<AudioClip> levelBGMAudioClips;
-    [SerializeField] private List<AudioClip> gameOverAudioClips;
     [SerializeField] private List<AudioClip> otherAudioClips;
     // private GameObject _player;
+    private List<AsyncOperation> _scenesLoading;
     private AudioSource _audioSource;
+    private EventSystem _eventSystem;
+
+    private void Awake()
+    {
+        if (_gameManagerInstance != null && _gameManagerInstance != this)
+            Destroy(gameObject);
+        else
+            _gameManagerInstance = this;
+
+        _eventSystem = FindObjectOfType<EventSystem>();
+    }
+
     private void Start()
     {
         DontDestroyOnLoad(gameObject);
         _audioSource = GetComponent<AudioSource>();
+        _scenesLoading = new List<AsyncOperation>();
     }
 
     private void Update()
     {
             
     }
-    
-    
+
+    public void DeselectButton()
+    {
+        if (_eventSystem == null)
+            return;
+        _eventSystem.SetSelectedGameObject(null);
+    }
+
+    public void QuitGame()
+    {
+        _audioSource.enabled = false;
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        var lvl01AudioClipIndex = Random.Range(0, GameConstants.Level01AudioClipLength);
+        _audioSource.clip = levelBGMAudioClips[lvl01AudioClipIndex];
+        _audioSource.enabled = true;
+        
+        _scenesLoading.Add(SceneManager.UnloadSceneAsync(GameConstants.SceneMainMenu));
+        // TODO change scene name later
+        _scenesLoading.Add(SceneManager.LoadSceneAsync(GameConstants.SceneLose));
+    }
+
+    public void PauseGame()
+    {
+        
+    }
+
+    public void SettingsMenu()
+    {
+        
+    }
 }
