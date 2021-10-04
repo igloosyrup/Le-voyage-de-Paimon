25f76b153bd3bb4859ffdd9ba22923dcf1fc1d1f
IglooSyrup <darckuvina@gmail.com>
2021-10-04 01:48:00 -0400

    Scripts - Ajout de toute la fonctionalité NextLevel

--- Le voyage de Paimon/Assets/Scripts/Canvas/PauseGame.cs
+++ Le voyage de Paimon/Assets/Scripts/Canvas/PauseGame.cs
@@ -1,18 +1,74 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class PauseGame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // private static PauseGame _pauseGameInstance;
+    // public static PauseGame GetInstancePauseGame => _pauseGameInstance;
+    //
+    [SerializeField] private GameObject pauseMenu;
+    private bool _isGamePause;
+    private GameManager _gameManager;
+    
+    // private void Awake()
+    // {
+    //     if (_pauseGameInstance != null && _pauseGameInstance != this)
+    //         Destroy(gameObject);
+    //     else
+    //         _pauseGameInstance = this;
+    // }
+
+    private void Start()
+    {
+        _gameManager = GameManager.GetGameManagerInstance;
+    }
+
+    private void Update()
+    {
+        if (!Input.GetKeyDown(KeyCode.Escape)) return;
+        if (_isGamePause)
+            Resume();
+        else
+            Pause();
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        _isGamePause = false;
+    }
+
+    private void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        _isGamePause = true;
+    }
+
+    public void MainMenu()
+    {
+        Resume();
+        _gameManager.OnNextScene += GoToMainMenu;
+    }
+
+    private string GoToMainMenu()
+    {
+        return GameConstants.SceneMainMenu;
+    }
+    
+    public void Settings()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool GetGameStatus()
     {
-        
+        return _isGamePause;
+    }
+
+    private void OnDestroy()
+    {
+        _gameManager.OnNextScene -= GoToMainMenu;
     }
 }
